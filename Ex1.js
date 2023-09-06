c2f43a6a39205be1349ee5304816234de1a8c93b
Vladimir_rekai <vladimir.rekai.etu.unicaen.fr>
2023-09-06 10:44:29 +0200

    debut git

---
+++ Ex1.js
@@ -0,0 +1,15 @@
+let celsus = 30
+
+let fahrenheit = celsus * 9/5 + 32
+
+let maDiv = document.getElementById("Exercice_1") //recupère l'element div de la page web 
+
+maDiv.textContent = celsus// change le contenu du texte de la div de la page web 
+maDiv.textContent += " "
+maDiv.textContent += fahrenheit
+
+let largeur =10
+let longeur = 10
+
+let maDiv2= document.getElementById("Exercice_2")
+maDiv2.textContent=
---
+++ test.js
@@ -0,0 +1,8 @@
+let variable = "Hello Christophe"
+
+console.log(variable);
+let maDiv = document.getElementById("test") //recupère l'element div de la page web 
+
+maDiv.textContent = variable   // change le contenu du texte de la div de la page web 
+
+//Exercice 1
