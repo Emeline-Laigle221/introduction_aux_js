06b76a87808ed38b02ecf5e5a3b4e2bf4ae3e35c
Vladimir_rekai <vladimir.rekai.etu.unicaen.fr>
2023-09-06 11:46:43 +0200

    exo_4_partie_object

--- Partie_objet.js
+++ Partie_objet.js
@@ -14,7 +14,23 @@ console.log(personne.ville)
 
 //Ex2
 
-const personne2 =[{prenom: "John", nom: "Doe", age: 25, ville: "Rouen"},{prenom: "Jane", nom: "Doe", age: 30, ville: "Paris"},{prenom: "Jim", nom: "Doe", age: 35, ville: "Caen"}];
-console.log(personne2[0])//affiche juste object puis l'objet
-console.table(personne2[1])//affiche l'objet sous forme de tableaux
-console.table(personne2[2])
+const personnes =[{prenom: "John", nom: "Doe", age: 25, ville: "Rouen"},{prenom: "Jane", nom: "Doe", age: 30, ville: "Paris"},{prenom: "Jim", nom: "Doe", age: 35, ville: "Caen"}];
+/*console.log(personnes[0])//affiche juste object puis l'objet
+console.table(personnes[1])//affiche l'objet sous forme de tableaux
+console.table(personnes[2])*/
+
+//Ex3
+personnes.push({prenom : "Marc", nom: "Doe", age: 32, ville : "Marseille"}) 
+/*console.log(personnes)
+console.table(personnes)*/
+
+//Ex4
+
+personnes[0].prenom="Jean"
+personnes[0].nom="philippe"
+personnes[0].age="80"
+personnes[0].ville="caen"
+
+personnes.splice(1,1)
+console.log(personnes)
+console.table(personnes)
